2dd44282a4118ca46751f0e0f4426368ccc22838
Pratham Vasa <prathamvasa9@gmail.com>
2016-04-01 18:11:30 -0700

    updated

---
+++ store/app/controllers/application_controller.rb
@@ -0,0 +1,32 @@
+class ApplicationController < ActionController::Base
+	
+	before_action :authorize
+
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  # helper_method :current_user
+
+  protected
+	
+	def authorize
+    unless User.find_by(session[:user_id])
+      redirect_to login_url, notice: "Please log in"
+    else
+      @is_admin_true = true
+    end
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate_user!
+    unless current_user
+      redirect_to root_url
+    end
+  end
+
+end
---
+++ store/test/helpers/shopper_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class ShopperHelperTest < ActionView::TestCase
+end
